3ae297ec41b51923fe6b910bd36ae8fa3b651577
周揚凱 <brian950703@gmail.com>
2019-10-10 21:17:07 -0400

    add comment

--- LA.py
+++ LA.py
@@ -4,10 +4,10 @@ from weight import *
 def LA(G):
 	clusters = []
 	vertex = orderVertex(G)
-	# iterate through each vertex
+	# Iterate through each vertex
 	for v in vertex:
 		add = False
-		# iterate through all existing cluster to search if current vertex belongs to one of them
+		# Iterate through all existing cluster to search if current vertex belongs to one of them
 		for j in range(len(clusters)):
 			U = clusters[j] + [v]
 			UW = float(2 * nx.number_of_edges(G.subgraph(U)) / nx.number_of_nodes(G.subgraph(U)))
@@ -15,9 +15,9 @@ def LA(G):
 			if UW > W:
 				clusters[j] += [v]
 				add = True
-		# if the vertex doesn't belong to one of the existing cluster, create a new cluster
+		# If the vertex doesn't belong to one of the existing cluster, create a new cluster
 		if add == False:
 			clusters.append([v])
-	# return a list of cluster 
+	# Return a list of cluster 
 	return clusters
 
